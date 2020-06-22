56d3cf72da41f685c4a39682e6b63c0ff03de10f
dkibogy <jepkemboikibogy5@gmail.com>
2020-06-22 09:32:19 +0300

    add router outlet to the app component html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { link } from 'fs';
 import { UserComponent } from './user/user.component';
 import { RepoComponent } from './repo/repo.component';
 
@@ -10,6 +9,7 @@ import { RepoComponent } from './repo/repo.component';
 const routes: Routes = [
   { path: 'user-link', component: UserComponent },
   { path: 'repo-link', component: RepoComponent},
+  { path: '', redirectTo: 'user-link', pathMatch: 'full'}
 
 ];
 
