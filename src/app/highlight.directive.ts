99f6ee90e05db4880be7a0890dab0f1cf426a638
dkibogy <jepkemboikibogy5@gmail.com>
2020-06-23 02:24:03 +0300

    add highlight directive for hover

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
+
+  @HostListener('mouseover') mouseover() {
+    this.hover("underline")
+  }
+  @HostListener('mouseout') mouseout() {
+    this.hover("none")
+  }
+
+  private hover(action: string){
+    this.elem.nativeElement.style.textDecoration=action;
+  }
 
 }
